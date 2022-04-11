5d84c53ce91ec8d5eaa35c919eabdbdb2c4522e9
rogerbenedik <rogerbenedik@hotmail.com>
2022-04-11 18:36:54 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let velocity = 101
+
+if(velocity < 40){
+    console.log("estamos em uma velocidade aceitavel!");
+} else if(velocity <= 60){
+    console.log("esta em velocidade de risco")
+} else {
+    console.log("velocidade de  risco")
+}
+
+(velocity > 100) ? console.log("velocidade maior que 100") : console.log("velocidade menor que 100")
