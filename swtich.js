96e98dcff7032c2d4c3cd03d565b300963e3e49c
rogerbenedik <rogerbenedik@hotmail.com>
2022-04-13 12:30:43 -0400

    learning about estruturas de repetiçao

--- swtich.js
+++ swtich.js
@@ -1,3 +1,37 @@
+let distanceInLy = prompt("Digite a distancia em anos luz");
+let chosenOption = prompt(
+  "Para qual unidade deseja converter? \n1. Parse(pc) \n2. Unidade Astronomica (AU)" +
+    "\n3. Quilomentro (km)\n\n(Digite o numero da opçao desejada)"
+);
+let chosenUnity;
+let convertedDistance;
+
+switch (chosenOption) {
+  case "1":
+    chosenUnity = "Parsec";
+    convertedDistance = distanceInLy * 0.306601;
+    break;
+  case "2":
+    chosenUnity = "Unidade Astronomica";
+    convertedDistance = distanceInLy * 63241.1;
+    break;
+  case "3":
+    chosenUnity = "Quilomentro";
+    convertedDistance = distanceInLy * 9.5 * Math.pow(10, 12);
+    break;
+  default:
+    chosenOption = "unidade não indentificada";
+    convertedDistance = "Conversão fora do escopo";
+}
+alert(
+  "Distancia em Anos-Luz:" +
+    distanceInLy +
+    "\n" +
+    chosenUnity +
+    ": " +
+    convertedDistance
+);
+
 // let velocity = 80;
 
 // switch (velocity * 2) {
@@ -41,19 +75,19 @@
 //     console.log("Velocidade não indentifica");
 // }
 
-let spaceship = "Elemental";
-switch (spaceship) {
-  case "golias":
-    console.log("nave mais resistente");
-  case "Elemental":
-    console.log("nave com melhor aramento");
-  case "helmet":
-    console.log("Rapida ativação de escudo");
-  case "frontier":
-    console.log("nave de infantaria");
-    break;
-  case "enterprise":
-    console.log("nave da frota estralar");
-  default:
-    console.log("nao conheço a nave");
-}
+// let spaceship = "Elemental";
+// switch (spaceship) {
+//   case "golias":
+//     console.log("nave mais resistente");
+//   case "Elemental":
+//     console.log("nave com melhor aramento");
+//   case "helmet":
+//     console.log("Rapida ativação de escudo");
+//   case "frontier":
+//     console.log("nave de infantaria");
+//     break;
+//   case "enterprise":
+//     console.log("nave da frota estralar");
+//   default:
+//     console.log("nao conheço a nave");
+// }
