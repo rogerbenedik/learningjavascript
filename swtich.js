dd31d1c30bd17b711393e2e6ca8f688f149ec4b5
rogerbenedik <rogerbenedik@hotmail.com>
2022-04-13 11:34:58 -0400

    switch cases

--- swtich.js
+++ swtich.js
@@ -1,13 +1,59 @@
-let velocity = 80;
+// let velocity = 80;
 
-switch (velocity * 2) {
-  case 100:
-    console.log("sua velocidade é 100km/s");
+// switch (velocity * 2) {
+//   case 100:
+//     console.log("sua velocidade é 100km/s");
+//     break;
+//   case 160:
+//     console.log("sua velocidade é 160/s");
+//     break;
+//   default:
+//     console.log("Velocidade não indenficada");
+// }
+
+// let spaceship = "starwar";
+
+// switch (spaceship) {
+//   case "Elemental":
+//     console.log("Nave mais resisnte");
+//     break;
+//   case "Raptor":
+//     console.log("Nave mais rápida");
+//     break;
+//   case "Enterprise":
+//     console.log("Nave a frota Estelar");
+//     break;
+//   default:
+//     console.log("Nave comun");
+// }
+
+// let velocity = 150;
+// switch (velocity) {
+//   case 80:
+//   case 90:
+//   case 100:
+//     console.log("velocidade aceitavel");
+//     break;
+//   case 110:
+//     console.log("velocidade alta, mas aceitável");
+//     break;
+//   default:
+//     console.log("Velocidade não indentifica");
+// }
+
+let spaceship = "Elemental";
+switch (spaceship) {
+  case "golias":
+    console.log("nave mais resistente");
+  case "Elemental":
+    console.log("nave com melhor aramento");
+  case "helmet":
+    console.log("Rapida ativação de escudo");
+  case "frontier":
+    console.log("nave de infantaria");
     break;
-  case 160:
-    console.log("sua velocidade é 160/s");
-    break;
+  case "enterprise":
+    console.log("nave da frota estralar");
   default:
-    console.log("Velocidade não indenficada");
+    console.log("nao conheço a nave");
 }
- 
