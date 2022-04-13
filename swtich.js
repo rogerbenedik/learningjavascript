483134956bb2ed3a81b2ef747a16c77eec5f06b7
rogerbenedik <rogerbenedik@hotmail.com>
2022-04-13 11:12:45 -0400

    learning switch

---
+++ swtich.js
@@ -0,0 +1,13 @@
+let velocity = 80;
+
+switch (velocity * 2) {
+  case 100:
+    console.log("sua velocidade é 100km/s");
+    break;
+  case 160:
+    console.log("sua velocidade é 160/s");
+    break;
+  default:
+    console.log("Velocidade não indenficada");
+}
+ 
