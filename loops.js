1a41cf46639e7caf7336ee7d0b5f721be7027fd5
rogerbenedik <rogerbenedik@hotmail.com>
2022-04-13 13:47:43 -0400

    loops with for, possivel adicionar a varival que utilizaremos para controlar as iterações

---
+++ loops.js
@@ -0,0 +1,13 @@
+let spaceshipName = "eeee"
+let newSpaceshipName = ""
+
+for(let i = 0; i < spaceshipName.length; i++){
+    if(spaceshipName[i] == "e"){
+        newSpaceshipName += "i"
+    }else{ 
+    newSpaceshipName += spaceshipName[i]
+    }
+}
+
+console.log(newSpaceshipName)
+
