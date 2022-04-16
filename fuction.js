557ee19607602840962f8ca8d6785d0d5efeb989
rogerbenedik <rogerbenedik@hotmail.com>
2022-04-16 14:11:10 -0400

    scope

--- fuction.js
+++ fuction.js
@@ -1,50 +1,61 @@
-let spaceshipName = prompt("Digite nome da Nave");
-let spaceshipVelocity = 0;
-let chosenOption;
+let spaceshipName = "SuperNova"
+function changeSpaceshipName(){
+  spaceshipName = "mars"
+}
+console.log(spaceshipName)
+changeSpaceshipName()
+console.log(spaceshipName)
 
-function showMenu() {
-  let option;
-  while (option != "1" && option != "2" && option != "3" && option != "4") {
-    option = prompt(
-      "O que deseja Fazer?\n" +
-        "1- Acelerar a nave em 5Km/s \n" +
-        "2- Desacelerar a nave em 5km/s\n" +
-        "3- Imprimir dados de bordo\n" +
-        "4- Sair de programa"
-    );
-  }
-  return option;
-}
-function speedUp(velocity) {
-  let newVelocity = velocity + 5;
-  return newVelocity;
-}
-function slowdown(velocity) {
-  let newVelocity = velocity - 5;
-  if (newVelocity < 0) {
-    newVelocity = 0;
-  }
-  return newVelocity;
-}
-function printSpaceshipBoardData(name, velocity) {
-  alert("espaçonave" + name + "\n Velocidade: " + velocity + "Km/s");
-}
-do {
-  chosenOption = showMenu();
-  switch (chosenOption) {
-    case "1":
-      spaceshipVelocity = speedUp(spaceshipVelocity);
-      break;
-    case "2":
-      spaceshipVelocity = slowdown(spaceshipVelocity);
-      break;
-    case "3":
-      printSpaceshipBoardData(spaceshipName, spaceshipVelocity);
-      break;
-    default:
-      alert("Encerrando programa de bordo");
-  }
-} while (chosenOption != "4");
+
+
+
+// let spaceshipName = prompt("Digite nome da Nave");
+// let spaceshipVelocity = 0;
+// let chosenOption;
+
+// function showMenu() {
+//   let option;
+//   while (option != "1" && option != "2" && option != "3" && option != "4") {
+//     option = prompt(
+//       "O que deseja Fazer?\n" +
+//         "1- Acelerar a nave em 5Km/s \n" +
+//         "2- Desacelerar a nave em 5km/s\n" +
+//         "3- Imprimir dados de bordo\n" +
+//         "4- Sair de programa"
+//     );
+//   }
+//   return option;
+// }
+// function speedUp(velocity) {
+//   let newVelocity = velocity + 5;
+//   return newVelocity;
+// }
+// function slowdown(velocity) {
+//   let newVelocity = velocity - 5;
+//   if (newVelocity < 0) {
+//     newVelocity = 0;
+//   }
+//   return newVelocity;
+// }
+// function printSpaceshipBoardData(name, velocity) {
+//   alert("espaçonave" + name + "\n Velocidade: " + velocity + "Km/s");
+// }
+// do {
+//   chosenOption = showMenu();
+//   switch (chosenOption) {
+//     case "1":
+//       spaceshipVelocity = speedUp(spaceshipVelocity);
+//       break;
+//     case "2":
+//       spaceshipVelocity = slowdown(spaceshipVelocity);
+//       break;
+//     case "3":
+//       printSpaceshipBoardData(spaceshipName, spaceshipVelocity);
+//       break;
+//     default:
+//       alert("Encerrando programa de bordo");
+//   }
+// } while (chosenOption != "4");
 
 // function speedUp (velocity, acceleration){
 //   let newVelocity = velocity + acceleration
